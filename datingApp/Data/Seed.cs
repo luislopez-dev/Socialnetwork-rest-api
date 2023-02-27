39c18aee0eb2756962c0cdbdb94cead650120cca
Luis Lopez <luis.dev.master@gmail.com>
2023-02-27 09:57:41 -0600

    Adding roles

--- datingApp/Data/Seed.cs
+++ datingApp/Data/Seed.cs
@@ -1,15 +1,16 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
+using Azure.Core;
 using datingApp.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace datingApp.Data;
-
+    
 public class Seed
 {
-    public static async Task SeedUsers(UserManager<AppUser> userManager)
+    public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync()) return;
 
@@ -17,10 +18,29 @@ public class Seed
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
 
+        var roles = new List<AppRole>
+        {
+            new AppRole { Name = "Member" },
+            new AppRole { Name = "Admin" },
+            new AppRole { Name = "Moderator" }
+        };
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
+            
         foreach (var user in users)
         {
             user.UserName = user.UserName.ToLower();
             await userManager.CreateAsync(user, "Pa$$w0rd");
+            await userManager.AddToRoleAsync(user, "Member");
         }
+
+        var admin = new AppUser
+        {
+            UserName = "admin"
+        };
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        await userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" });
     }
 }
